2cd3338cb6d3e854f19ed0c456aa001e0ab2a36d
Dioumel <khadydioumel9@gmail.com>
2020-11-27 00:30:56 +0100

    ajout modification de khady dioumel

---
+++ projetGL/src/main/java/com/classe/Calculator.java
@@ -0,0 +1,42 @@
+package com.classe;
+
+import java.util.Arrays;
+
+public class Calculator
+{
+    public int sum(int a, int b)
+    {
+        return a+b;
+    }
+    
+      public  int minus(int a, int b)
+    {
+        return a-b;
+    }
+      
+    public int divide(int a, int b)
+    {
+        return a/b;
+    }
+    public int multiply(int a, int b)
+    {
+        return a*b;
+    }
+    
+    public int min(int a, int b)
+    {
+        if(a<=b)
+           return a;
+        else 
+            return b;
+    }
+    
+    public int max(int a, int b)
+    {
+         if(a<=b)
+           return b;
+        else 
+            return a;
+    }
+    
+}
---
+++ projetGL/src/test/java/com/classe/CalculatorTest.java
@@ -0,0 +1,48 @@
+package com.classe;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase  {
+	
+	Calculator cal;
+	@Before
+	public void setUp() throws Exception {
+		cal = new Calculator();
+	}
+
+	@Test
+	public void testSum() {
+		Assert.assertEquals(4, cal.sum(2, 2));
+	}
+	
+	@Test
+	public void testMinus() {
+		Assert.assertEquals(0, cal.minus(2, 2));
+	}
+	
+	@Test
+	public void testDivide() {
+		Assert.assertEquals(2, cal.divide(6, 3));
+	}
+	@Test
+	public void testMultiply(){
+		Assert.assertEquals(12, cal.multiply(3,4));
+	}
+
+	@Test
+	public void testMin(){
+		int min = cal.min(10,20);
+		if (min != 10) Assert.fail();
+	}
+
+	@Test 
+	public void testMax(){
+		int max = cal.max(2000, -2000);
+		Assert.assertEquals(2000,max);
+
+	
+}
